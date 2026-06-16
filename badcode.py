7af8a1e388617b5466fd76460712dbc356d8d153
prestonM3 <prestonm6140@gmail.com>
2026-06-16 14:18:41 -0400

    added more "wet" comments

--- badcode.py
+++ badcode.py
@@ -1,19 +1,19 @@
 class Calculator:
     # add two numbers
     def addition(self, input1, input2):
-        x = input1 + input2
-        return x
+        x = input1 + input2 # add input 1 and 2
+        return x # return x
     
     # subtract two numbers
     def subtractingtwonumbers(self, k, l): 
-        thisistheresult = k * l
+        thisistheresult = k * l # multiply k and l 
         return  thisistheresult
 
     #multiplying two numbers
     def multiply(self, input1, input2):
         b = ""
         if input2 == 0:
-            return 0
+            return 0 # return 0
         return a + multiply(input1, input2 - 1)
         
     def tell_me_a_story(self):
