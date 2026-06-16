60d2e8056e217abdeb84b880b509dac05766c5dd
Shen <shane.yang@yahoo.com>
2026-06-16 13:45:18 -0400

    finished making a simple calculator class

--- goodcode.py
+++ goodcode.py
@@ -2,3 +2,15 @@
 
 #adding a single repsonsiblity
 
+class Calculator(Calculator):
+    def add(self, input1, input2):
+        return input1 + input2
+    def substract(self, input1, input2):
+        return input1 - input2
+    def multiply(self, input1, input2):
+        return input1 * input2
+    def divide(self, input1, input2):
+        return input1 / input2
+
+
+print(Calculator().add(1, 2))
