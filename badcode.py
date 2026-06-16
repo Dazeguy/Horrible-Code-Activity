f80d67afae3d13d61240f56dbfe93ce6b4da391b
prestonM3 <prestonm6140@gmail.com>
2026-06-16 14:10:47 -0400

    finalized bad code

--- badcode.py
+++ badcode.py
@@ -8,6 +8,18 @@ class Calculator:
     def subtractingtwonumbers(self, k, l): 
         thisistheresult = k * l
         return  thisistheresult
+
+    #multiplying two numbers
+    def multiply(self, input1, input2):
+        b = ""
+        if input2 == 0:
+            return 0
+        return a + multiply(input1, input2 - 1)
+        
+    def tell_me_a_story(self):
+        print("Once upon a time there was a calculator that did way too much.")
+
+
     
 
 if __name__ == "__main__":
