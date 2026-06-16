2ab56a77d07482ecb32aad2d53ad249d9f29f859
Shen <shane.yang@yahoo.com>
2026-06-16 14:50:35 -0400

    completed good code single responsbilit

--- goodcode.py
+++ goodcode.py
@@ -3,14 +3,19 @@
 #adding a single repsonsiblity
 
 class Calculator(Calculator):
+    #addition
     def add(self, input1, input2):
         return input1 + input2
+
+    #subtraction
     def substract(self, input1, input2):
         return input1 - input2
+
+    #multiplication
     def multiply(self, input1, input2):
         return input1 * input2
+
+    #division
     def divide(self, input1, input2):
         return input1 / input2
 
-
-print(Calculator().add(1, 2))
